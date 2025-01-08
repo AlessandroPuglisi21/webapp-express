9de96d50ef56dc8db2e6ce9b0bd3ac3f5b5b53e1
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-01-08 16:30:30 +0100

    ADD: Index && Show Request

--- app.js
+++ app.js
@@ -3,12 +3,15 @@ const app = express()
 const port = 3000
 const notFound = require('./middlewares/notFound')
 const errorsHandler = require ('./middlewares/errorsHandler')
+const moviesRouter = require ('./routers/moviesRouters')
 
 app.use(express.static('public'))
 
 app.get('/',(req,res) => {
     res.send('Server Attivo')
 })
+
+app.use('/api/movies',moviesRouter)
 
 app.use(errorsHandler)
 
