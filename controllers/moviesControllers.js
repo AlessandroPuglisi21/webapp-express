7e9778569628e3ba1a0f6409abfb9978ab83e721
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-01-09 17:07:50 +0100

    ADD:  Filter

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -1,42 +1,41 @@
 const connection = require("../data/db")
 
-function index (req, res) {
+// Funzione per ottenere la lista dei film
+function index(req, res) {
+    const searchTerm = req.query.search || ''; 
+    const sql = `SELECT * FROM movies_db.movies WHERE title LIKE ?`; 
 
-    const sql = `SELECT * FROM movies_db.movies;`
+    connection.query(sql, [`%${searchTerm}%`], (err, movies) => {
+        if (err) return res.status(500).json({ message: err.message });
 
-    connection.query(sql, (err,movies) => {
-        if(err) return res.status(500).json({message: err.message})
-
-            res.json(movies)
-    })
+        res.json(movies); 
+    });
 }
 
-function show (req, res) {
+function show(req, res) {
+    const id = req.params.id;
 
-    const id = req.params.id
-
-    const sql = `SELECT * FROM movies WHERE id = ?`
+    const sql = `SELECT * FROM movies WHERE id = ?`;
 
     connection.query(sql, [id], (err, results) => {
-        if(err) return res.status(500).json({ message: err.message })
-            if(results.length === 0 )
-                return res.status (404).json({
-                    error: 'Not Found',
-                    message: 'Movies Not Found :('
-            })
+        if (err) return res.status(500).json({ message: err.message });
+        if (results.length === 0)
+            return res.status(404).json({
+                error: 'Not Found',
+                message: 'Movie Not Found :(',
+            });
 
-            const book = results [0]
+        const movie = results[0];
 
-            const sql = `SELECT * FROM reviews WHERE movie_id = ?`
+        const sqlReviews = `SELECT * FROM reviews WHERE movie_id = ?`;
 
-            connection.query(sql, [id], (err, results) => {
-                if (err) return res.status(500).json({ message: err.message })
+        connection.query(sqlReviews, [id], (err, reviews) => {
+            if (err) return res.status(500).json({ message: err.message });
 
-                    book.reviews = results
-                    res.json(book)
-            })
-        })
-
+            movie.reviews = reviews;
+            res.json(movie);
+        });
+    });
 }
 
-module.exports = { index, show}
+module.exports = { index, show };
