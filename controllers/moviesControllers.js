e58112d704249d7e57c75590646051c03e924a0c
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2025-01-13 17:04:10 +0100

    ADD: Query review

--- controllers/moviesControllers.js
+++ controllers/moviesControllers.js
@@ -8,12 +8,18 @@ function index(req, res) {
     connection.query(sql, [`%${searchTerm}%`], (err, movies) => {
         if (err) return res.status(500).json({ message: err.message });
 
-        res.json(movies); 
+        res.json(movies.map(movie => {
+            return {
+                ...movie,
+                image: 'http://localhost:3000' + movie.image
+            }
+        })); 
     });
 }
 
 function show(req, res) {
     const id = req.params.id;
+    
 
     const sql = `SELECT * FROM movies WHERE id = ?`;
 
@@ -38,4 +44,27 @@ function show(req, res) {
     });
 }
 
-module.exports = { index, show };
+function movieReview (req,res) {
+    const id = req.params.id
+
+    const {text, vote, name} = req.body 
+
+    console.log( id, text, vote, name)
+    const intVote = parseInt(vote)
+
+    if(!intVote || isNaN(intVote) || !name) {
+        return res.status(400).json({error: 'Missing data'})
+    }
+
+    //query Insert into 
+
+    const sql = `INSERT INTO reviews (text, vote, name, movie_id) VALUES (?, ?, ?, ?)`
+    connection.query(sql, [text, intVote, name, id], (err, results) => {
+        if (err) return res.status(500).json({ message: err.message });
+        console.log(results)
+
+        res.status(201).json({ message: 'Review added' });
+    });
+}
+
+module.exports = { index, show, movieReview };
